722577ab96bb6480819c4b2b3fcdeb765ec83fce
FacundoRinon <facuzionff@gmail.com>
2023-12-11 13:59:42 -0300

    navBar ready (avatar static)

--- movieFront/src/pages/Home/index.jsx
+++ movieFront/src/pages/Home/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+import Navbar from "../../components/Navbar";
+
 import "./index.scss";
 
 const Home = () => {
   return (
     <div className="home">
+      <Navbar />
       <h1>Home page</h1>
     </div>
   );
