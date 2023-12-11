064d102a6da42c3bbc189553a0a54306cbae798c
FacundoRinon <facuzionff@gmail.com>
2023-12-11 11:15:04 -0300

    login page ready

--- movieFront/src/pages/Login/index.jsx
+++ movieFront/src/pages/Login/index.jsx
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 
 import "./index.scss";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await axios({
+      method: "POST",
+      url: `${import.meta.env.VITE_API_URL}/users/login`,
+      data: {
+        email: emailValue,
+        password: passwordValue,
+      },
+    });
+    if (response.data.token) {
+      dispatch(setToken(response.data));
+      navigate("/");
+    }
+  }
+
   return (
     <div className="login">
-      <h1>Login page</h1>
+      <div className="login__content">
+        <div className="login__img">
+          <img src="../public/logo.png" alt="" />
+        </div>
+        <div className="login__container">
+          <h1>Welcome Back</h1>
+          <form id="form" method="post" action="" onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              type="text"
+              placeholder="example@gmail.com"
+              value={emailValue}
+              onChange={(event) => setEmailValue(event.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Your password"
+              value={passwordValue}
+              onChange={(event) => setPasswordValue(event.target.value)}
+            />
+            <button>Create your IMDb account</button>
+          </form>
+          <p>
+            Don´t have an account? <Link to={"/signUp"}>Sign up</Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- movieFront/src/pages/SignUp/index.jsx
+++ movieFront/src/pages/SignUp/index.jsx
@@ -35,47 +35,49 @@ const SignUp = () => {
 
   return (
     <div className="signUp">
-      <div className="signUp__img">
-        <img src="../public/logo.png" alt="" />
-      </div>
-      <div className="signUp__container">
-        <h2>Create account</h2>
-        <form id="form" method="post" action="" onSubmit={handleSubmit}>
-          <label htmlFor="name">Your name</label>
-          <input
-            id="name"
-            type="text"
-            placeholder="First and last name"
-            value={nameValue}
-            onChange={(event) => setNameValue(event.target.value)}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="text"
-            value={emailValue}
-            onChange={(event) => setEmailValue(event.target.value)}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            placeholder="at least 8 characters"
-            value={passwordValue}
-            onChange={(event) => setPasswordValue(event.target.value)}
-          />
-          <label htmlFor="password2">Re-enter password</label>
-          <input
-            id="password2"
-            type="password"
-            value={password2Value}
-            onChange={(event) => setPassword2Value(event.target.value)}
-          />
-          <button>Create your IMDb account</button>
-        </form>
-        <p>
-          Already have an account? <Link>Log in</Link>
-        </p>
+      <div className="signUp__content">
+        <div className="signUp__img">
+          <img src="../public/logo.png" alt="" />
+        </div>
+        <div className="signUp__container">
+          <h1>Create account</h1>
+          <form id="form" method="post" action="" onSubmit={handleSubmit}>
+            <label htmlFor="name">Your name</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="First and last name"
+              value={nameValue}
+              onChange={(event) => setNameValue(event.target.value)}
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              type="text"
+              value={emailValue}
+              onChange={(event) => setEmailValue(event.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="at least 8 characters"
+              value={passwordValue}
+              onChange={(event) => setPasswordValue(event.target.value)}
+            />
+            <label htmlFor="password2">Re-enter password</label>
+            <input
+              id="password2"
+              type="password"
+              value={password2Value}
+              onChange={(event) => setPassword2Value(event.target.value)}
+            />
+            <button>Create your IMDb account</button>
+          </form>
+          <p>
+            Already have an account? <Link to={"/login"}>Log in</Link>
+          </p>
+        </div>
       </div>
     </div>
   );
